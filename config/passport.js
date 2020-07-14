cc25b7e63ec1e75edb09a04250aa74faac5d6a2a
Kaio Pomini <kaiopomini@gmail.com>
2020-07-13 23:35:34 -0300

    handled auth

--- config/passport.js
+++ config/passport.js
@@ -5,8 +5,8 @@ const keys = require('./keys')
 module.exports = function(passport) {
   passport.use (
     new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: keys.googleClientID || process.env.ClientID,
+      clientSecret: keys.googleClientSecret || process.env.clientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
