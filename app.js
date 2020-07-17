6174844118fb0cbd9b88287a4ae9a2869de2e4a6
Kaio Pomini <kaiopomini@gmail.com>
2020-07-16 22:14:37 -0300

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
 
+// To put keys from heroku environment and hide on github
 require('dotenv').config()
 
 // Passport Config
