f80a63efc5b966ff23f86ee9e8f9657c15748af0
Kaio Pomini <kaiopomini@gmail.com>
2020-10-20 22:47:09 -0300

    StoryForm and restricting routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
@@ -20,6 +21,7 @@ require('./config/passport')(passport)
 // Load Routes
 const index = require('./routes/index')
 const auth = require('./routes/auth')
+const stories = require('./routes/stories')
 
 
 //Map global promises
@@ -60,9 +62,13 @@ app.use((req, res, next) => {
   next()
 })
 
+// set static folder
+app.use(express.static(path.join(__dirname,'public')))
+
 // use Routes
 app.use('/', index)
 app.use('/auth', auth)
+app.use('/stories', stories)
 
 
 const port = process.env.PORT || 5000
